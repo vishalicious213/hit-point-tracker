087452fb36d2b61feb07f6b00fea8181eb44ac3a
vishalicious213 <vishalicious213@gmail.com>
2023-02-06 19:50:56 -0800

    added event listener to form and addNewCharacter() function

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hit point tracker")
+const characters = document.getElementById("characters")
+const addCharForm = document.getElementById("add-char-form")
+
+addCharForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    addNewCharacter()
+    console.log("submitted")
+})
+
+function addNewCharacter(newChar) {
+    characters.innerHTML += `
+    <div class="char">
+        <h2 class="name">Character name</h2>
+        <div class="health">
+            <div><span class="current-hp">100</span> / <span class="total-hp">100</span></div>
+        </div>
+        <div class="char-buttons">
+            <button class="health-button add-button" type="button">+</button>
+            <button class="health-button sub-button" type="button">-</button>
+        </div>
+    </div>
+    `
+}
