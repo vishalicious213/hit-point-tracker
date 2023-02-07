204bc3238d81c71daf4be167a5563a4c5757a2fc
vishalicious213 <vishalicious213@gmail.com>
2023-02-06 20:14:50 -0800

    added current hp to the form, event handler and addNewChar()

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const characters = document.getElementById("characters")
 const addCharForm = document.getElementById("add-char-form")
 const addNameBtn = document.getElementById("add-name")
-const addHpBtn = document.getElementById("add-max-hp")
+const addCurHpBtn = document.getElementById("add-current-hp")
+const addMaxHpBtn = document.getElementById("add-max-hp")
 
 addCharForm.addEventListener("submit", function(e) {
     e.preventDefault()
     const newChar = {
         name: addNameBtn.value,
-        maxHp: addHpBtn.value
+        curHp: addCurHpBtn.value,
+        maxHp: addMaxHpBtn.value
     }
     addNewCharacter(newChar)
     addNameBtn.value = ""
-    addHpBtn.value = ""
+    addCurHpBtn.value = ""
+    addMaxHpBtn.value = ""
     console.log("submitted")
 })
 
@@ -21,7 +24,7 @@ function addNewCharacter(newChar) {
         <div class="char">
             <h2 class="name">${newChar.name}</h2>
             <div class="health">
-                <div><span class="current-hp">100</span> / <span class="total-hp">${newChar.maxHp}</span></div>
+                <div><span class="current-hp">${newChar.curHp}</span> / <span class="total-hp">${newChar.maxHp}</span></div>
             </div>
             <div class="char-buttons">
                 <button class="health-button add-button" type="button">+</button>
