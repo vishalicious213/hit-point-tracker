7bc3455bfc9767c64395f407aa73e8505a3507c0
vishalicious213 <vishalicious213@gmail.com>
2023-02-09 15:14:50 -0800

    handleMove() moves the selected character to the left!

--- index.js
+++ index.js
@@ -174,6 +174,9 @@ function handleMove(char, direction) {
         console.log(char, direction)
         const tempCharacter = charactersArray[characterIndex -1]
         console.log(tempCharacter)
+        charactersArray[characterIndex -1] = charToMove
+        charactersArray[characterIndex] = tempCharacter
+        renderCharacters(charactersArray)
     }
 
     if (direction === "right") {
